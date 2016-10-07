0407b5eb43719a071472ae31faea95a0ede6b85b
Zack Ginies <zack@zackginies.com>
2016-10-06 22:19:20 -0400

    Setup ajax calls to data

--- assets/javascripts/index.coffee
+++ assets/javascripts/index.coffee
@@ -1,7 +1,20 @@
+# TODO: make this way better than it currently is lollllz
+
 
 class @BlackHoof
   @init: ->
     @getFood()
+    @getHours()
+    @getCocktails()
+    @getRedWine()
+    @getWhiteWine()
+  @getHours: ->
+    $.ajax(
+      type: "GET",
+      url:  "https://the-black-hoof.firebaseio.com/hoof_hours.json",
+      success: (data) =>
+        @buildHours(data)
+    )
   @getFood: ->
     $.ajax(
       type: "GET",
@@ -9,11 +22,46 @@ class @BlackHoof
       success: (data) =>
         @buildFood(data)
     )
+  @getCocktails: ->
+    $.ajax(
+      type: "GET",
+      url:  "https://the-black-hoof.firebaseio.com/hoof_cocktails.json",
+      success: (data) =>
+        @buildCocktails(data)
+    )
+  @getRedWine: ->
+    $.ajax(
+      type: "GET",
+      url:  "https://the-black-hoof.firebaseio.com/hoof_red_wine.json",
+      success: (data) =>
+        @buildRedWine(data)
+    )
+  @getWhiteWine: ->
+    $.ajax(
+      type: "GET",
+      url:  "https://the-black-hoof.firebaseio.com/hoof_white_wine.json",
+      success: (data) =>
+        @buildWhiteWine(data)
+    )
+  @buildHours: (hours) ->
+    $hoursTable = $('.hoof-info-hours')
+    $.each hours, (i, val) ->
+      $hoursTable.append('<tr><td>' + val.day + '</td><td>' + val.hours + '</td></tr>')
   @buildFood: (items) ->
     $foodMenu = $('.hoof-menu-food-list')
     $.each items, (i, val) ->
       $foodMenu.append('<li><h3>' + val.name + '<span>' + val.price + '</span></h3></li>')
-      console.log(val)
-    console.log(items)
+  @buildCocktails: (items) ->
+    $cocktailMenu = $('.hoof-menu-cocktails-list')
+    $.each items, (i, val) ->
+      $cocktailMenu.append('<li><h3 class="serif">' + val.name + '<span>' + val.price + '</span></h3><p>' + val.description + '</p></li>')
+  @buildRedWine: (items) ->
+    $redWineMenu = $('.hoof-menu-red-wine-list')
+    $.each items, (i, val) ->
+      $redWineMenu.append('<li><h3>' + val.name + '</h3><p>' + val.description + '<span>' + val.price + '</span></p></li>')
+  @buildWhiteWine: (items) ->
+    $whiteWineMenu = $('.hoof-menu-white-wine-list')
+    $.each items, (i, val) ->
+      $whiteWineMenu.append('<li><h3>' + val.name + '</h3><p>' + val.description + '<span>' + val.price + '</span></p></li>')
 
 window.BlackHoof.init()
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,7 +2,23 @@ this.BlackHoof = (function() {
   function BlackHoof() {}
 
   BlackHoof.init = function() {
-    return this.getFood();
+    this.getFood();
+    this.getHours();
+    this.getCocktails();
+    this.getRedWine();
+    return this.getWhiteWine();
+  };
+
+  BlackHoof.getHours = function() {
+    return $.ajax({
+      type: "GET",
+      url: "https://the-black-hoof.firebaseio.com/hoof_hours.json",
+      success: (function(_this) {
+        return function(data) {
+          return _this.buildHours(data);
+        };
+      })(this)
+    });
   };
 
   BlackHoof.getFood = function() {
@@ -17,14 +33,80 @@ this.BlackHoof = (function() {
     });
   };
 
+  BlackHoof.getCocktails = function() {
+    return $.ajax({
+      type: "GET",
+      url: "https://the-black-hoof.firebaseio.com/hoof_cocktails.json",
+      success: (function(_this) {
+        return function(data) {
+          return _this.buildCocktails(data);
+        };
+      })(this)
+    });
+  };
+
+  BlackHoof.getRedWine = function() {
+    return $.ajax({
+      type: "GET",
+      url: "https://the-black-hoof.firebaseio.com/hoof_red_wine.json",
+      success: (function(_this) {
+        return function(data) {
+          return _this.buildRedWine(data);
+        };
+      })(this)
+    });
+  };
+
+  BlackHoof.getWhiteWine = function() {
+    return $.ajax({
+      type: "GET",
+      url: "https://the-black-hoof.firebaseio.com/hoof_white_wine.json",
+      success: (function(_this) {
+        return function(data) {
+          return _this.buildWhiteWine(data);
+        };
+      })(this)
+    });
+  };
+
+  BlackHoof.buildHours = function(hours) {
+    var $hoursTable;
+    $hoursTable = $('.hoof-info-hours');
+    return $.each(hours, function(i, val) {
+      return $hoursTable.append('<tr><td>' + val.day + '</td><td>' + val.hours + '</td></tr>');
+    });
+  };
+
   BlackHoof.buildFood = function(items) {
     var $foodMenu;
     $foodMenu = $('.hoof-menu-food-list');
-    $.each(items, function(i, val) {
-      $foodMenu.append('<li><h3>' + val.name + '<span>' + val.price + '</span></h3></li>');
-      return console.log(val);
+    return $.each(items, function(i, val) {
+      return $foodMenu.append('<li><h3>' + val.name + '<span>' + val.price + '</span></h3></li>');
     });
-    return console.log(items);
+  };
+
+  BlackHoof.buildCocktails = function(items) {
+    var $cocktailMenu;
+    $cocktailMenu = $('.hoof-menu-cocktails-list');
+    return $.each(items, function(i, val) {
+      return $cocktailMenu.append('<li><h3 class="serif">' + val.name + '<span>' + val.price + '</span></h3><p>' + val.description + '</p></li>');
+    });
+  };
+
+  BlackHoof.buildRedWine = function(items) {
+    var $redWineMenu;
+    $redWineMenu = $('.hoof-menu-red-wine-list');
+    return $.each(items, function(i, val) {
+      return $redWineMenu.append('<li><h3>' + val.name + '</h3><p>' + val.description + '<span>' + val.price + '</span></p></li>');
+    });
+  };
+
+  BlackHoof.buildWhiteWine = function(items) {
+    var $whiteWineMenu;
+    $whiteWineMenu = $('.hoof-menu-white-wine-list');
+    return $.each(items, function(i, val) {
+      return $whiteWineMenu.append('<li><h3>' + val.name + '</h3><p>' + val.description + '<span>' + val.price + '</span></p></li>');
+    });
   };
 
   return BlackHoof;
